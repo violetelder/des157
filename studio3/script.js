529595b427dd99bcdaf9bea7e17293320e8e8ce1
violet elder <vmelder@ucdavis.edu>
2018-02-26 22:31:16 -0800

    Moodboard and project brief
    Moodboard, moodboard pictures, and project brief files.

--- studio3/script.js
+++ studio3/script.js
@@ -11,6 +11,13 @@ var containerh3 = document.getElementById("containerh3");
 //get container1 h3 so i can change it on drop
 var container1h3 = document.getElementById("container1h3");
 
+//get container2 so i can change it on drop
+var target2 = document.getElementById("target2");
+//get container2 h3 so i can change it on drop
+var container3h3 = document.getElementById("container3h3");
+//get container1 h3 so i can change it on drop
+var container2h3 = document.getElementById("container2h3");
+
 //trying to drag and drop again!
 //allow drop event
 function allowDrop(ev) {
@@ -20,6 +27,7 @@ function allowDrop(ev) {
 //drag event
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
+    ev.dataTransfer.setData("text", ev.target2.id);
 }
 
 //drop event
@@ -27,8 +35,10 @@ function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
+    ev.target2.appendChild(document.getElementById(data));
+    container1h3.innerHTML=" ";
     container1h3.innerHTML=" ";
     target.style.backgroundColor="yellow";
-    containerh3.style.color="cornflowerblue";
-    containerh3.innerHTML="you did it!";
+    container2h3.style.color="cornflowerblue";
+    container2h3.innerHTML="you did it! again!";
   }
